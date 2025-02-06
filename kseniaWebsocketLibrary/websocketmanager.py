8a3a5e4c316bda254d0d25b33d5c148927de2240
realnot16 <bennypadula@gmail.com>
2025-02-06 21:50:15 +0100

    fix systme

--- kseniaWebsocketLibrary/websocketmanager.py
+++ kseniaWebsocketLibrary/websocketmanager.py
@@ -411,6 +411,7 @@ class WebSocketManager:
 
     #this function get system status from ws
     async def getSystem(self):
+        self._logger.debug(f"trying adding system {self._readData['STATUS_SYSTEM']} {self._readData}")
 
         sysList = self._readData["STATUS_SYSTEM"]
         systems = []
